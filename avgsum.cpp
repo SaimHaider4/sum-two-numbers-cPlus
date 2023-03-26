59f18c029b6c52f9518cdce2134179c21858bae6
Saim <syedsaim.h123@gmail.com>
2023-03-26 05:59:38 +0500

    First Commit

---
+++ avgsum.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+float a,b,c;
+cout<<"Enter 1st number "<<endl;
+cin>>a;
+cout<<" Enter 2nd number  "<<endl;
+cin>>b;
+cout<<" Enter the 3rd number "<<endl;
+cin>>c;
+float sum=a+b+c;
+cout<<"The sum is  "<<sum<<endl;
+ float avg=sum/3;
+ cout<<" THe average is "<<avg;
+return 0;
+
+
+}
